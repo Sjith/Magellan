Sjith/Magellan

commit 29a521e95d16928c4aa12e58ef360df345335ef9

Magellan is now using its own Adapter for the list view. This adapter will give us in the future more flexibility to display various icons, such as thumbnails.

src/com/quasarlab/magellan/ListItemAdapter.java
@@ -1,0 +1,73 @@
+package com.quasarlab.magellan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ListItemAdapter extends BaseAdapter
+{
+	private Context m_context;
+	private List<MagellanFile> m_items = new ArrayList<MagellanFile>();
+	
+	public ListItemAdapter(Context context)
+	{
+		m_context = context;
+	}
+	
+	public void addItem(MagellanFile f)
+	{
+		m_items.add(f);
+	}
+	
+	public void setItemList(List<MagellanFile> list)
+	{
+		m_items = list;
+	}
+	
+	@Override
+	public int getCount() 
+	{
+		return m_items.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) 
+	{
+		return m_items.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) 
+	{
+		return arg0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) 
+	{
+		ListItem it;
+		if(arg1 == null)
+			it = new ListItem(m_context, m_items.get(arg0));
+		else
+			it = (ListItem) arg1;
+		
+		MagellanFile f = m_items.get(arg0);
+		it.setTitle(f.getName());
+		
+		it.setDescr( f.isDirectory() ? 
+			String.format(m_context.getResources().getString(R.string.mainactivity_folder_description), MainActivity.convert(new Folder(f.getAbsolutePath()).recursiveSize(), m_context)) :
+			String.format(m_context.getResources().getString(R.string.mainactivity_file_description), MainActivity.convert(f.length(), m_context)));
+		
+		it.setDescr(String.format(m_context.getResources().getString(R.string.mainactivity_file_description), MainActivity.convert(f.length(), m_context)));
+		
+		int drawable_id = f.isDirectory() ? R.drawable.folder : R.drawable.file;
+		it.setIcon(m_context.getResources().getDrawable(drawable_id));
+		
+		return it;
+	}
+
+}

src/com/quasarlab/magellan/AsyncDetailsDisplayer.java
@@ -49,12 +49,10 @@ public class AsyncDetailsDisplayer extends AsyncTask<String, Void, Void> {
 		catch (IllegalArgumentException e) {
 			// Window has leaked
 		}
-		
-		MainActivity act = (MainActivity) m_context;
-		
+				
 		AlertDialog.Builder adb = new AlertDialog.Builder(m_context);
 		adb.setTitle(file.getName());
-		adb.setMessage(String.format(m_context.getResources().getString(R.string.mainactivity_dialog_details_info), file.getAbsolutePath(), act.convert(size), count, type));
+		adb.setMessage(String.format(m_context.getResources().getString(R.string.mainactivity_dialog_details_info), file.getAbsolutePath(), MainActivity.convert(size, m_context), count, type));
 		adb.setPositiveButton(m_context.getResources().getString(R.string.error_dialog_ok), null);
 		adb.show();
 	}

src/com/quasarlab/magellan/ListItem.java
@@ -1,0 +1,74 @@
+package com.quasarlab.magellan;
+
+import com.quasarlab.magellan.Folder;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ListItem extends LinearLayout
+{
+	private TextView m_title;
+	private TextView m_descr;
+	private ImageView m_icon;
+	private Context m_context;
+	
+	public ListItem(Context context, MagellanFile f)
+	{
+		super(context);
+		m_context = context;
+		
+		LayoutInflater layout = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		layout.inflate(R.layout.item, this, true);
+		
+		m_title = (TextView) findViewById(R.id.title);
+		m_descr = (TextView) findViewById(R.id.descr);
+		m_icon = (ImageView) findViewById(R.id.img);
+		
+		setFile(f);
+	}
+	
+	public void setFile(MagellanFile f)
+	{
+		m_title.setText(f.getName());
+		
+		// icon and descr
+		Drawable d;
+		String descr;
+		
+		if(f.isDirectory())
+		{
+			d = m_context.getResources().getDrawable(R.drawable.folder);
+			descr = String.format(m_context.getResources().getString(R.string.mainactivity_folder_description), MainActivity.convert(new Folder(f.getAbsolutePath()).recursiveSize(), m_context));
+		}
+		else
+		{
+			d = m_context.getResources().getDrawable(R.drawable.file);
+			descr = String.format(m_context.getResources().getString(R.string.mainactivity_file_description), MainActivity.convert(f.length(), m_context));
+		}
+		
+		m_descr.setText(descr);
+		m_icon.setImageDrawable(d);
+	}
+	
+	public void setTitle(String title)
+	{
+		m_title.setText(title);
+	}
+	
+	public void setDescr(String descr)
+	{
+		m_descr.setText(descr);
+	}
+	
+	public void setIcon(Drawable d)
+	{
+		m_icon.setImageDrawable(d);
+	}
+}
+

src/com/quasarlab/magellan/MainActivity.java
@@ -1,5 +1,6 @@
 package com.quasarlab.magellan;
 
+import java.util.List;
 import java.util.Vector;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +10,7 @@ import java.io.IOException;
 import com.quasarlab.magellan.R;
 import com.quasarlab.magellan.CopyOperation;
 import com.quasarlab.magellan.Folder;
+import com.quasarlab.magellan.MagellanFile;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -33,6 +35,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.EditText;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -46,18 +49,18 @@ public class MainActivity extends Activity
 	String m_copied;
 	String m_clickedFile;
 	
-	public String convert(long size)
+	static public String convert(long size, Context c)
 	{
 		String ret;
 		
 		if(size/1000000000 > 0) // superior to 1 GB
-			ret = String.valueOf((double) Math.round((((double)size) / ((double)1000000000))*10) / 10 ) + " " + getResources().getString(R.string.unit_gigabyte);  
+			ret = String.valueOf((double) Math.round((((double)size) / ((double)1000000000))*10) / 10 ) + " " + c.getResources().getString(R.string.unit_gigabyte);  
 		else if(size/1000000 > 0) // superior to 1 MB
-			ret = String.valueOf((double) Math.round((((double)size) / ((double)1000000))*10) / 10 ) + " " + getResources().getString(R.string.unit_megabyte);  
+			ret = String.valueOf((double) Math.round((((double)size) / ((double)1000000))*10) / 10 ) + " " + c.getResources().getString(R.string.unit_megabyte);  
 		else if(size/1000 > 0) // superior to 1KB
-			ret = String.valueOf((double) Math.round((((double)size) / ((double)1000))*10) / 10 ) + " " + getResources().getString(R.string.unit_kilobyte);  
+			ret = String.valueOf((double) Math.round((((double)size) / ((double)1000))*10) / 10 ) + " " + c.getResources().getString(R.string.unit_kilobyte);  
 		else
-			ret = String.valueOf(size) + " " + getResources().getString(R.string.unit_byte);
+			ret = String.valueOf(size) + " " + c.getResources().getString(R.string.unit_byte);
 		
 		return ret;
 	}
@@ -134,40 +137,47 @@ public class MainActivity extends Activity
 		/* put them in our item model, folders first */
 		HashMap<String, String> map;
 		ArrayList<HashMap<String, String> > itemList = new ArrayList<HashMap<String,String>>();
-
+		
+		ListItemAdapter ad = new ListItemAdapter(this);
+		List<MagellanFile> list = new ArrayList<MagellanFile>();
 		for(int i = 0; i < foldersList.size(); i++)
 		{
 			String name = foldersList.get(i);
-			Folder folder = new Folder(m_currentPath + "/" + name);
+//			Folder folder = new Folder(m_currentPath + "/" + name);
+//
+//			map = new HashMap<String,String>();
+//			map.put("title", name);
+//			
+//			String descr = String.format(getResources().getString(R.string.mainactivity_folder_description, folder.count(), folder.count()));
+//			map.put("descr", descr);
+//			
+//			map.put("img", String.valueOf(R.drawable.folder));
+//			itemList.add(map);
+			
 
-			map = new HashMap<String,String>();
-			map.put("title", name);
-			
-			String descr = String.format(getResources().getString(R.string.mainactivity_folder_description, folder.count(), folder.count()));
-			map.put("descr", descr);
-			
-			map.put("img", String.valueOf(R.drawable.folder));
-			itemList.add(map);
+			MagellanFile f = new MagellanFile(m_currentPath + "/" + name);
+			ad.addItem(f);
 		}
 
 		/* then simple files */
 		for(int i = 0; i < filesList.size(); i++)
 		{
 			String name = filesList.get(i);
-			File file = new File(m_currentPath + "/" + name);
-			
-			map = new HashMap<String,String>();
-			map.put("title", name);
-			
-			String descr = String.format(getResources().getString(R.string.mainactivity_file_description), convert(file.length()));
-			map.put("descr", descr);
-			
-			map.put("img", String.valueOf(R.drawable.file));
-			itemList.add(map);
+//			File file = new File(m_currentPath + "/" + name);
+//			
+//			map = new HashMap<String,String>();
+//			map.put("title", name);
+//			
+//			String descr = String.format(getResources().getString(R.string.mainactivity_file_description), convert(file.length(), this));
+//			map.put("descr", descr);
+//			
+//			map.put("img", String.valueOf(R.drawable.file));
+//			itemList.add(map);
+			MagellanFile f = new MagellanFile(m_currentPath + "/" + name);
+			ad.addItem(f);
 		}		
 
-		SimpleAdapter adapter = new SimpleAdapter(this.getBaseContext(), itemList, R.layout.item, new String[] {"img", "title", "descr"}, new int[] {R.id.img, R.id.title, R.id.descr});
-		m_listView.setAdapter(adapter);
+		m_listView.setAdapter(ad);
 	}
 
 	public void populateBreadcrumb() {
@@ -235,8 +245,7 @@ public class MainActivity extends Activity
 			@SuppressWarnings("unchecked")
 			public void onItemClick(AdapterView<?> a, View v, int position, long id) 
 			{ 
-				HashMap<String,String> map = (HashMap<String,String>) m_listView.getItemAtPosition(position);
-				MagellanFile clickedFile = new MagellanFile(m_currentPath + "/" + map.get("title"));
+				MagellanFile clickedFile = (MagellanFile) m_listView.getItemAtPosition(position);
 				if(clickedFile.isDirectory())
 				{
 					// do we have permissions to explore this directory ?
@@ -277,8 +286,7 @@ public class MainActivity extends Activity
 		int position = ((AdapterContextMenuInfo)menuInfo).position;
 		
 		/* did the user clicked on a directory or a file ? get the file clicked */
-		HashMap<String,String> map = (HashMap<String,String>) m_listView.getItemAtPosition(position);
-		File clickedFile = new File(m_currentPath + "/" + map.get("title"));
+		MagellanFile clickedFile = (MagellanFile) m_listView.getItemAtPosition(position);
 		menu.setHeaderTitle(clickedFile.getName());
 		
 		/* use the appropriate menu : simple files have a share action */
@@ -510,8 +518,7 @@ public class MainActivity extends Activity
 	public boolean onContextItemSelected(MenuItem item) 
 	{
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-		HashMap<String,String> map = (HashMap<String,String>) m_listView.getItemAtPosition(info.position);
-		File clickedFile = new File(m_currentPath + "/" + map.get("title"));
+		File clickedFile = (MagellanFile) m_listView.getItemAtPosition(info.position);
 		m_clickedFile = clickedFile.getAbsolutePath();
 		
 		switch (item.getItemId()) 

commit 642d5e2deea99a12fdcd505f448162f6fdb357c8

Fix compilation (broken since commit 29a521e).

src/com/quasarlab/magellan/ListItemAdapter.java
@@ -28,19 +28,16 @@ public class ListItemAdapter extends BaseAdapter
 		m_items = list;
 	}
 	
-	@Override
 	public int getCount() 
 	{
 		return m_items.size();
 	}
 
-	@Override
 	public Object getItem(int arg0) 
 	{
 		return m_items.get(arg0);
 	}
 
-	@Override
 	public long getItemId(int arg0) 
 	{
 		return arg0;
@@ -51,7 +48,6 @@ public class ListItemAdapter extends BaseAdapter
 		return m_items;
 	}
 	
-	@Override
 	public View getView(int arg0, View arg1, ViewGroup arg2) 
 	{
 		ListItem it;